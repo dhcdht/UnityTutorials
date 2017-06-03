de26ead1320cea33d20a0a0885f251e4b402c7ec
dhcdht <dhcdht@qq.com>
2017-06-03 09:16:32 +0800

    init and 1RollABall

---
+++ Assets/1RollABall/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float Speed;
+	public Text CountText;
+	public Text WinText;
+
+	private Rigidbody _rb;
+	private int _count;
+
+	// Use this for initialization
+	private void Start()
+	{
+		_rb = GetComponent<Rigidbody>();
+		_count = 0;
+		WinText.text = "";
+		
+		SetCountText();
+	}
+
+	private void FixedUpdate()
+	{
+		var moveHorizontal = Input.GetAxis("Horizontal");
+		var moveVertical = Input.GetAxis("Vertical");
+
+		var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		_rb.AddForce(movement * Speed);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!other.gameObject.CompareTag("Pick Up")) return;
+		
+		other.gameObject.SetActive(false);
+		_count += 1;
+		SetCountText();
+	}
+
+	private void SetCountText()
+	{
+		CountText.text = string.Format("Count: {0}", _count);
+		if (_count >= 3)
+		{
+			WinText.text = "You win!";
+		}
+	}
+}
