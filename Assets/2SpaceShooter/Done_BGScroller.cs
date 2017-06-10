59cd699b4b25003bf5e67e764db5bd1d61e8d348
dhcdht <dhcdht@qq.com>
2017-06-10 22:03:01 +0800

    2SpaceShooter 移动的背景

---
+++ Assets/2SpaceShooter/Done_BGScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace _2SpaceShooter
+{
+	public class Done_BGScroller : MonoBehaviour {
+
+		public float ScrollSpeed;
+		public float TileSizeZ;
+
+		private Vector3 _startPosition;
+
+		// Use this for initialization
+		void Start () {
+			_startPosition = transform.position;
+		}
+
+		// Update is called once per frame
+		void Update () {
+			float newPosition = Mathf.Repeat(Time.time * ScrollSpeed, TileSizeZ);
+			transform.position = _startPosition + Vector3.forward * newPosition;
+		}
+	}
+}
