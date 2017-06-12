dd9f1977acb60222a98c065c83cd09e186a897c3
dhcdht <dhcdht@qq.com>
2017-06-12 20:53:59 +0800

    2SpaceShooter 子弹飞行

---
+++ Assets/2SpaceShooter/Done_Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Done_Mover : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
