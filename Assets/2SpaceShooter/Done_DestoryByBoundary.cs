474fe229136c70402910d598c37102b19de19c05
dhcdht <dhcdht@qq.com>
2017-06-14 21:01:26 +0800

    2SpaceShooter 添加 boundary，超出范围销毁对象

---
+++ Assets/2SpaceShooter/Done_DestoryByBoundary.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _2SpaceShooter
+{
+    public class Done_DestoryByBoundary : MonoBehaviour
+    {
+        private void OnTriggerExit(Collider other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/2SpaceShooter/Done_GameController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _2SpaceShooter
+{
+    public class Done_GameController : MonoBehaviour
+    {
+        public GameObject[] hazards;
+        public Vector3 spawnValues;
+        public int hazardCount;
+        public float spawnWait;
+        public float startWait;
+        public float waveWait;
+
+        public GUIText gameOverText;
+        public GUIText restartText;
+        public GUIText scoreText;
+
+        private bool gameOver;
+        private bool restart;
+        private int score;
+
+        private void Start()
+        {
+            gameOver = false;
+            restart = false;
+            score = 0;
+
+            gameOverText.text = "";
+            restartText.text = "";
+            UpdateScore();
+
+            StartCoroutine(SpawnWaves());
+        }
+
+        private void Update()
+        {
+            if (restart)
+            {
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+            }
+        }
+
+        private IEnumerator SpawnWaves()
+        {
+            yield return new WaitForSeconds(startWait);
+            while (true)
+            {
+                for (int i = 0; i < hazardCount; i++)
+                {
+                    GameObject hazard =
+                        hazards[Random.Range(0, hazards.Length)];
+                    Vector3 spawnPosition =
+                        new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y,
+                            spawnValues.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(hazard, spawnPosition, spawnRotation);
+                    yield return new WaitForSeconds(spawnWait);
+                }
+                yield return new WaitForSeconds(waveWait);
+
+                if (gameOver)
+                {
+                    restartText.text = "Press 'R' for Restart";
+                    restart = true;
+                    break;
+                }
+            }
+        }
+
+        private void UpdateScore()
+        {
+            scoreText.text = "Score: " + score;
+        }
+
+        // MARK: - Public Methods
+
+        public void AddScore(int newScoreValue)
+        {
+            score += newScoreValue;
+            UpdateScore();
+        }
+
+        public void GameOver()
+        {
+            gameOverText.text = "Game Over!";
+            gameOver = true;
+        }
+    }
+}
