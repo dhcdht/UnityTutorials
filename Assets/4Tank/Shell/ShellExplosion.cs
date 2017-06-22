7f68a302d74c34d28ca69ef322f525a3c458bbd0
dhcdht <dhcdht@qq.com>
2017-06-22 20:12:44 +0800

    4Tank 坦克发射炮弹及其相关控制的一部分

---
+++ Assets/4Tank/Shell/ShellExplosion.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_tankMask;
+    public ParticleSystem m_explosionParticles;
+    public AudioSource m_explosionAudio;
+    public float m_maxDamage = 100.0f;
+    public float m_explosionForce = 1000.0f;
+    public float m_MaxLifeTime = 2.0f;
+    public float m_explosionRadius = 5.0f;
+
+    private void Start()
+    {
+        // 炮弹存活到最长时间后销毁它
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var colliders = Physics.OverlapSphere(transform.position, m_explosionRadius, m_tankMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            var targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRigidbody)
+            {
+                continue;
+            }
+            targetRigidbody.AddExplosionForce(m_explosionForce, transform.position, m_explosionRadius);
+
+            // todo: 坦克伤害
+        }
+
+        m_explosionParticles.transform.parent = null;
+        m_explosionParticles.Play();
+        m_explosionAudio.Play();
+
+        var mainModule = m_explosionParticles.main;
+        Destroy(m_explosionParticles.gameObject, mainModule.duration);
+
+        Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        var explosionToTarget = targetPosition - transform.position;
+        var explosionDistance = explosionToTarget.magnitude;
+        var relativeDistance = (m_explosionRadius - explosionDistance) / m_explosionRadius;
+        var damage = relativeDistance * m_maxDamage;
+        damage = Mathf.Max(0.0f, damage);
+
+        return damage;
+    }
+}
