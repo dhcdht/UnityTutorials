72a03e77816bec2d33e8204c819137a6611c73f0
dhcdht <dhcdht@qq.com>
2017-06-27 20:30:48 +0800

    3ServivalShooter CameraFollow

---
+++ Assets/3ServivalShooter/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5.0f;
+
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var targetCameraPosition = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime);
+    }
+}
